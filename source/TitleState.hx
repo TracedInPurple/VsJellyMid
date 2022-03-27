6f263ea0bdf6800bc02fef927d0d3e550d004014
TheGaboDiaz <90982287+TheGaboDiaz@users.noreply.github.com>
2022-03-27 18:36:18 -0500

    title

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -157,7 +157,7 @@ class TitleState extends MusicBeatState
 			FlxG.sound.music.fadeIn(4, 0, 0.7);
 		}
 
-		Conductor.changeBPM(102);
+		Conductor.changeBPM(72);
 		persistentUpdate = true;
 
         pano = new FlxSprite(-1600, 0).loadGraphic(Paths.image('titleBG'));
@@ -176,7 +176,7 @@ class TitleState extends MusicBeatState
 		// bg.updateHitbox();
 		//add(bg);
 
-		logoBl = new FlxSprite(110, 0).loadGraphic(Paths.image('jellymid'));
+		logoBl = new FlxSprite(110, -250).loadGraphic(Paths.image('jellymid'));
 		logoBl.antialiasing = true;
 		logoBl.updateHitbox();
 		logoBl.scale.set(0.6, 0.6);
